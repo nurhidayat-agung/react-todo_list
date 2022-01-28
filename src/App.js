751c6060314ff7ee5329ec3f4f917f664f4e089e
agung.hidayat <agung.hidayat@kalbenutritionals.com>
2022-01-28 10:31:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Category from './pages/Category'
+import ToDoList from './pages/ToDoList'
 
 function App() {
+  const menus = [
+    { id: 0, path: '/home', component: Home},
+    { id: 1, path: '/about', component: About},
+    { id: 2, path: '/category', component: Category},
+    { id: 3, path: '/todolist', component: ToDoList},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/* <Route path="/home" component={Home} />
+        <Route path="/about" component={About} /> */}
+
+        {menus.map((menu)=>
+        <Route 
+          key={menu.id}
+          exact 
+          path={menu.path} 
+          component={menu.component}></Route>
+        )}
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/ToDoList/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function index() {
+  return <div>To Do List</div>;
+}
+
+export default index;
